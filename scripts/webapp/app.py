cac1719f6f51cc649304abb267904c2389f23ae6
Jason Yee <jyee@hellyeah.com>
2020-10-30 10:31:34 -0700

    fix bytestring issue in app

--- scripts/webapp/app.py
+++ scripts/webapp/app.py
@@ -6,7 +6,7 @@ from flask import request, redirect, render_template, url_for
 from flask import Response
 
 app = Flask(__name__)
-app.redis = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+app.redis = redis.StrictRedis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
 
 # Be super aggressive about saving for the development environment.
 # This says save every second if there is at least 1 change.  If you use
