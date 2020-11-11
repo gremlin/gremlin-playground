e19c8083018b26474417452637b4f1d0910f3513
Jason Yee <jyee@hellyeah.com>
2020-11-11 09:43:37 -0800

    App updates and helper tool
    - Updated the app to include post timestamps
    - added traffic generation helper tool
    - restructured the commands layout to make it easier to add helper tools

--- scripts/gremlin.sh
+++ scripts/gremlin.sh
@@ -5,6 +5,9 @@ curl https://rpm.gremlin.com/gremlin.repo -o /etc/yum.repos.d/gremlin.repo
 # Install Gremlin client and daemon
 yum install -y gremlin gremlind
 
+# Move MOTD into place
 mv /tmp/motd /etc/motd
-mv /tmp/gremlin-playground /usr/bin/gremlin-playground
-chmod 755 /usr/bin/gremlin-playground
+
+# Move helper commands into run path
+chmod 755 /tmp/commands/*
+mv /tmp/commands/* /usr/bin/
--- scripts/webapp/app.py
+++ scripts/webapp/app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import redis
 
@@ -6,27 +7,29 @@ from flask import request, redirect, render_template, url_for
 from flask import Response
 
 app = Flask(__name__)
-app.redis = redis.StrictRedis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
+app.redis = redis.StrictRedis(host="127.0.0.1", port=6379, db=0, decode_responses=True)
 
 # Be super aggressive about saving for the development environment.
 # This says save every second if there is at least 1 change.  If you use
 # redis in production you'll want to read up on the redis persistence
 # model.
-app.redis.config_set('save', '1 1')
+app.redis.config_set("save", "1 1")
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def main_page():
-    if request.method == 'POST':
-        app.redis.lpush('entries', request.form['entry'])
-        return redirect(url_for('main_page'))
+    if request.method == "POST" and request.form["entry"].strip():
+        # Add the datetime to the message. This makes it easier to see updates.
+        message = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "> " + request.form["entry"].strip()
+        app.redis.lpush("entries", message)
+        return redirect(url_for("main_page"))
     else:
-        entries = app.redis.lrange('entries', 0, -1)
-        return render_template('main.html', entries=entries)
+        entries = app.redis.lrange("entries", 0, -1)
+        return render_template("main.html", entries=entries)
 
-@app.route('/clear', methods=['POST'])
+@app.route("/clear", methods=["POST"])
 def clear_entries():
-    app.redis.ltrim('entries', 1, 0)
-    return redirect(url_for('main_page'))
+    app.redis.ltrim("entries", 1, 0)
+    return redirect(url_for("main_page"))
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
+  app.run(host="0.0.0.0", port=80)
